180252f41a1a3e1983e62ec3df3b73f6b6c735c2
Joe Edelman <joe@citizenlogistics.com>
2013-08-09 14:19:28 -0700

    paints

--- src/upcoming_controller.rb
+++ src/upcoming_controller.rb
@@ -19,7 +19,7 @@ class UpcomingController < UICollectionViewController
 		legit_events(timeframe).each do |ev|
 			morning = ev.startDate.start_of_day
 			section = @events_by_day[morning] ||= []
-			section << ev unless section.detect{ |existing| existing.title == ev.title }
+			section << ev unless section.detect{ |existing| existing.title == ev.title and existing.startDate == ev.startDate }
 		end
 	end
 
@@ -51,10 +51,6 @@ class UpcomingController < UICollectionViewController
 	################
 	# transitions
 
-	def batch_updates &foo
-		collectionView.performBatchUpdates(proc{ foo.call(collectionView) }, completion:nil)
-	end
-
 	def only_when_editing
 		rm_sections = []
 		plus_indexes = []
@@ -65,32 +61,50 @@ class UpcomingController < UICollectionViewController
 				rm_sections << i
 			end
 		end
-		# puts "insertSections: #{new_sections.inspect}"
-		# puts "insertItemsAtIndexPaths: #{plus_indexes.inspect}"
 		return [plus_indexes, rm_sections]
 	end
 
-	def start_editing
-		@editing = true
-		plus_indexes, new_sections = *only_when_editing
+	def toggle_editing
+		@editing = !@editing
+		plus_indexes, extra_sections = *only_when_editing
 		batch_updates do |cv|
-			cv.insertSections(new_sections.nsindexset)
-			cv.insertItemsAtIndexPaths(plus_indexes.map(&:nsindexpath))
-		end
-	end
-
-	def stop_editing
-		@editing = false
-		plus_indexes, rm_sections = *only_when_editing
-		batch_updates do |cv|
-			cv.deleteItemsAtIndexPaths(plus_indexes.map(&:nsindexpath))
-			cv.deleteSections(rm_sections.nsindexset)
+			if @editing
+				cv.insertSections(extra_sections.nsindexset)
+				cv.insertItemsAtIndexPaths(plus_indexes.map(&:nsindexpath))
+			else
+				cv.deleteItemsAtIndexPaths(plus_indexes.map(&:nsindexpath))
+				cv.deleteSections(extra_sections.nsindexset)
+			end
 		end
 	end
 
 
-	############
-	# lifecycle
+
+	################
+	# calendar model
+
+	def add_event start_time, friend, title = nil
+		return AddressBook.pick do |person|
+			add_event(start_time, person, title)
+		end if friend == :pick
+
+		@event_store ||= EKEventStore.alloc.init
+		ev = EKEvent.eventWithEventStore(@event_store)
+		ev.startDate = start_time
+		ev.endDate = start_time + 2.hours
+		ev.title = title || (friend && "with #{friend.composite_name}") || "New event"
+		ev.setCalendar(@event_store.defaultCalendarForNewEvents)
+		error = Pointer.new('@')
+		@event_store.saveEvent(ev, span:EKSpanThisEvent, commit:true, error:error)
+		Event.assign(ev.eventIdentifier, friend) if friend
+	end
+
+	def fetch_events start_date, end_date
+		@event_store ||= EKEventStore.alloc.init
+	 	@event_store.requestAccessToEntityType(EKEntityTypeEvent, completion: nil);
+	 	p = @event_store.predicateForEventsWithStartDate(start_date, endDate: end_date, calendars: nil)
+		@event_store.eventsMatchingPredicate(p)
+	end
 
 	def legit_events(tf)
 		events = fetch_events(*tf) || []
@@ -103,6 +117,48 @@ class UpcomingController < UICollectionViewController
 			true
 		end
 	end
+
+	def day_of_week t
+		return "TODAY" if t.today?
+		return "TOMORROW" if t.same_day?(NSDate.tomorrow)
+		return t.strftime("%A     (%m/%d)").upcase
+	end
+
+	HOUR_RANGES = {
+		predawn: 0...5,
+		dawn: 5...8,
+		bfst: 8...10,
+		morn: 10...12,
+		lunch: 12...14,
+		aft: 14...16,
+		hpy_hr: 16...18,
+		dinner: 18...21,
+		night: 21...24
+	}
+
+	KEY_HOUR_RANGES = %w{ bfst morn lunch aft hpy_hr dinner night }
+
+	def time_of_day t
+		HOUR_RANGES.each do |k,v|
+			return k if v.include? t.hour
+		end
+	end
+
+	# def time_of_day t
+	# 	return 'DAWN' if t.hour < 8
+	# 	return 'BFST' if t.hour < 10
+	# 	return 'MORN'  if t.hour < 12
+	# 	return 'LUNCH' if t.hour < 14
+	# 	return 'AFT'   if t.hour < 16
+	# 	return 'HPY HR' if t.hour < 18
+	# 	return 'DINNER' if t.hour < 21
+	# 	return 'LATE'
+	# 	# .string_with_format("h:mma")
+	# end
+
+
+	############
+	# lifecycle
 
 	def viewDidLoad
 		super
@@ -127,87 +183,83 @@ class UpcomingController < UICollectionViewController
 		App.notification_center.unobserve @ekobserver if @ekobserver
 	end
 
-	def fetch_events start_date, end_date
-		@event_store ||= EKEventStore.alloc.init
-	 	@event_store.requestAccessToEntityType(EKEntityTypeEvent, completion: nil);
-	 	p = @event_store.predicateForEventsWithStartDate(start_date, endDate: end_date, calendars: nil)
-		@event_store.eventsMatchingPredicate(p)
-	end
-
 	def reload
   		load_events
   		collectionView.reloadData
 	end
 
 
+
 	############
 	# actions
 
-	def composeAction
-		if @editing
-			stop_editing
-			self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemEdit, target: self, action: :composeAction)
-		else
-			start_editing
-			self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemDone, target: self, action: :composeAction)
+	def choose_paint
+		# simple risky brainy active sweet outdoor quiet creative rowdy
+		menu2000 %w{ exercise creative sweet } do |paint|
+			puts "...got paint #{paint.inspect}"
+			paint_with paint if paint
 		end
 	end
 
-	def eventEditViewController(c, didCompleteWithAction: action)
-		dismissViewControllerAnimated true, completion: nil
+	def done_action
+		paint_with nil
+		toggle_editing if @editing
+		navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemEdit, target: self, action: :edit_action)
 	end
 
-	def compose_event start_time, person
-		@event_store ||= EKEventStore.alloc.init
-		ev = EKEvent.eventWithEventStore(@event_store)
-		ev.startDate = start_time
-		ev.endDate = start_time + 2.hours
-		c = EKEventEditViewController.alloc.init.tap do |c|
-			c.eventStore = @event_store
-			c.event = ev
-		    c.editViewDelegate = self
-		end
-    	presentViewController c, animated: true, completion: nil
+	def edit_action
+		toggle_editing unless @editing
+		navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemDone, target: self, action: :done_action)
 	end
 
-	def compose_event_with_picked_friend start_time
-  		AddressBook.pick do |person|
-  			return compose_event start_time unless person
-			@event_store ||= EKEventStore.alloc.init
-			ev = EKEvent.eventWithEventStore(@event_store)
-			ev.startDate = start_time
-			ev.endDate = start_time + 2.hours
-			ev.setCalendar(@event_store.defaultCalendarForNewEvents)
-  			error = Pointer.new('@')
-  			@event_store.saveEvent(ev, span:EKSpanThisEvent, commit:true, error:error)
-  			Event.assign(ev.eventIdentifier, person)
-  		end
+	def options_menu_for_event ev
+    	menu2000 ['Link friend', 'Hide'] do |pressed|
+		  	case pressed
+		  	when 'Hide';
+		  		Event.hide(ev.eventIdentifier)
+		  		reload
+		  	when 'Link friend';
+		  		AddressBook.pick do |person|
+		  			next unless person
+		  			Event.assign(ev.eventIdentifier, person)
+		  			collectionView.reloadItemsAtIndexPaths([path])
+		  		end
+		  	end
+    	end
 	end
 
+
+
+	############
+	# controller model stuff
+
+	def paint_with paint
+		return unless @paint = paint
+		puts "got paint #{paint.inspect}"
+		navigationController.navigationBar.prompt = paint && "Painting with #{paint}"
+		edit_action
+	end
 
 	def collectionView(cv, didSelectItemAtIndexPath:path)
 		kind, thing = thing_at_index_path path
 		case kind
 		when :event
 			return false unless ev = thing
+			return options_menu_for_event ev if @editing
 			eventViewController = EKEventViewController.alloc.init
 			eventViewController.event = ev
 			eventViewController.allowsEditing = true
 			navigationController.pushViewController(eventViewController, animated: true)
 		when :plus
 			date = thing
-			UIActionSheet.alert nil, buttons: ['Cancel', 'Lunch', 'Dinner'],
-			  cancel: proc { "hi" },
-			  success: proc { |pressed|
-			  	case pressed
-			  	when 'Lunch';
-			  		compose_event_with_picked_friend(date + 12.hours)
-			  		# compose_event date + 12.hours
-			  	when 'Dinner';
-			  		compose_event_with_picked_friend(date + 18.hours)
-			  		# compose_event date + 18.hours
-			  	end
-		  	  }
+			menu2000 KEY_HOUR_RANGES do |pressed|
+				range = HOUR_RANGES[pressed.to_sym]
+				next unless range
+				start_time = date + range.begin.hours
+		  		add_event(start_time, @paint ? nil : :pick, @paint)
+				paint_with nil if @paint
+				done_action
+		    end
 		end
 	end
 
@@ -219,22 +271,15 @@ class UpcomingController < UICollectionViewController
 		path = collectionView.indexPathForItemAtPoint(p)
     	kind, ev = thing_at_index_path(path)
     	return unless kind == :event and ev
+    	options_menu_for_event ev
+	end
 
-		UIActionSheet.alert nil, buttons: ['Cancel', 'Link friend', 'Hide'],
-		  cancel: proc { "hi" },
-		  success: proc { |pressed|
-		  	case pressed
-		  	when 'Hide';
-		  		Event.hide(ev.eventIdentifier)
-		  		reload
-		  	when 'Link friend';
-		  		AddressBook.pick do |person|
-		  			return unless person
-		  			Event.assign(ev.eventIdentifier, person)
-		  			collectionView.reloadItemsAtIndexPaths([path])
-		  		end
-		  	end
-	  	  }
+
+	##############
+	# misc controller wireups
+
+	def eventEditViewController(c, didCompleteWithAction: action)
+		dismissViewControllerAnimated true, completion: nil
 	end
 
 
@@ -287,25 +332,22 @@ class UpcomingController < UICollectionViewController
 	end
 
 
-	###############################
-	# image, time, and title logic
 
-	def day_of_week t
-		return "TODAY" if t.today?
-		return "TOMORROW" if t.same_day?(NSDate.tomorrow)
-		return t.strftime "%A     (%m/%d)"
+	##############
+	# improve uikit
+
+	def menu2000 options, &cb
+		puts "menu2000 called"
+		UIActionSheet.alert nil, buttons: ['Cancel', nil, *options], success: proc{ |thing|
+			puts "Got: #{thing.inspect}"
+			cb.call(thing) unless thing == 'Cancel' or thing == :Cancel
+		}, cancel: proc{
+			puts "cancel..."
+		}
 	end
 
-	def time_of_day t
-		return 'DAWN' if t.hour < 8
-		return 'BFST' if t.hour < 10
-		return 'MORN'  if t.hour < 12
-		return 'LUNCH' if t.hour < 14
-		return 'AFT'   if t.hour < 16
-		return 'HPY HR' if t.hour < 18
-		return 'DINNER' if t.hour < 21
-		return 'LATE'
-
-		# .string_with_format("h:mma")
+	def batch_updates &foo
+		collectionView.performBatchUpdates(proc{ foo.call(collectionView) }, completion:nil)
 	end
+
 end
